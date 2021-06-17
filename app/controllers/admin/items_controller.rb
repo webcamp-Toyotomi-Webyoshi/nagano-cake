32a00d0fb24b01c4d70566df49b298593a18b2c6
EC2 Default User <ec2-user@ip-172-31-10-48.ec2.internal>
2021-06-17 15:31:23 +0000

    [admin/items/show,edit][admin/genres/index/edit]

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,2 +1,34 @@
 class Admin::ItemsController < ApplicationController
+  def index
+    @items = Item.all
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    ridirect_to admin_item_path(@item)
+  end
+
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :image)
+  end
+
 end
