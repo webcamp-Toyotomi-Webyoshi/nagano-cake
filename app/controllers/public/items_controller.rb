638d1fbf38f725efc4c265a862fd8e213338e4ad
EC2 Default User <ec2-user@ip-172-31-10-48.ec2.internal>
2021-06-19 13:35:34 +0000

    public/items/index,show,bootstrap

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,2 +1,19 @@
 class Public::ItemsController < ApplicationController
+  PER = 6
+
+  def index
+    @items = Item.page(params[:page]).per(PER)
+  end
+
+  def show
+    @item = Item.find(params[:id])
+  end
+
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :introduction, :price, :image)
+  end
+
 end
