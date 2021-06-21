83f02f2a6b16bd3d0aebac61ac2794e83d79f9db
EC2 Default User <ec2-user@ip-172-31-10-48.ec2.internal>
2021-06-21 07:05:36 +0000

    カート機能

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,8 @@ class Customer < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :cart_items, dependent: :destroy
+  has_many :orders, dependent: :destroy
 
   def full_name
     self.first_name + self.last_name
