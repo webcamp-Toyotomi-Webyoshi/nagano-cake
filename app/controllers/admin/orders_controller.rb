9ef63c4c6d0c3c792d8bee23424233d60eaa0cb4
demi007 <gorigoriuhouho0820@gmail.com>
2021-06-21 05:54:21 +0000

    最新情報をpull(address)

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,2 +1,14 @@
 class Admin::OrdersController < ApplicationController
+
+  def index
+   @orders = Order.all
+  end
+  
+  def show
+   @order = Order.find(params[:id])
+  end
+  
+  def update
+   @order
+  end
 end
