00b8356609280a49c782e008bf3fac30b95a7951
demi007 <gorigoriuhouho0820@gmail.com>
2021-06-22 06:03:47 +0000

    pub/order/pull

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -3,12 +3,23 @@ class Admin::OrdersController < ApplicationController
   def index
    @orders = Order.all
   end
-  
-  def show
-   @order = Order.find(params[:id])
+
+  def customer_index
+   @customer = Customer.find(params[:id])
   end
-  
+
+  def show
+   #@order = Order.find(params[:id])
+  end
+
   def update
-   @order
+   @order = Order.find(params[:id])
+   @order.update(order_params)
+   redirect_to admin_order_path
+  end
+
+  private
+  def order_params
+   arams.require(:order).permit(:order_status)
   end
 end
