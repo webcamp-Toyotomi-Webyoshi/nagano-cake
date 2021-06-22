df049a8e3d13e5160002fab69fc775eaf91c8340
siryu-saito <saito.s.ryu@gmail.com>
2021-06-22 05:59:10 +0000

    エンドユーザ側 注文情報関係作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -12,14 +12,30 @@ class Public::OrdersController < ApplicationController
 
   def create
     @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.postage = 800
+    @order.total_payment = 500
+    @order.name = "ヤマぐり"
+    @order.address = ""
+    @order.post_cord = ""
+    @order.save
+    redirect_to orders_complete_path
   end
 
   def index
     @orders = current_customer.orders.all
   end
+  
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  def complete
+  end
+
 
   private
   def order_params
-    params.require(:order).permit(:payment_method, :address, :post_cord, :address, :name)
+    params.permit(:payment_method, :address, :post_cord, :name, :postage, :total_payment )
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,10 +1,8 @@
 class Order < ApplicationRecord
-  
+
   enum payment_method: { クレジットカード: 0, 銀行振込: 1 }
-  
-  
-  
+
   belongs_to :customer
   has_many :order_items, dependent: :destroy
-  
+
 end
