ddf1f674865146659a2e9c07cba2f126a7d19eb3
demi007 <gorigoriuhouho0820@gmail.com>
2021-06-24 01:45:06 +0000

    adminのafter_sign_inページ修正

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -18,9 +18,9 @@ class Admin::SessionsController < Devise::SessionsController
     # super
   # end
 
-  def after_sign_in_path_for(resource)
-    admin_customers_path
-  end
+  #def after_sign_in_path_for(resource)
+  #  admin_customers_path
+  #end
 
   # protected
 
